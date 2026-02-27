e22dabb3a997662b44eafdca8d9ac36c1b6ec3a1
BrijK <Brijk0625@gmail.com>
2026-02-27 20:55:52 +0530

    add basic Python examples and greeting function

---
+++ Basics-Of-Python/basic/basicThings_01.py
@@ -0,0 +1,30 @@
+# x = 10
+# print(type(x))
+
+# x = "User" #Dynamic typing
+# print(type(x))
+
+# x = int(input("Enter a number: "));
+# print(x)
+
+# x = (input("Enter a string : "));
+# print(x);
+
+# # Loops 
+
+# for i in range(5): #0 1 2 3 4 
+#     print(i);
+
+# print('-----')
+# for i in range(2,5): #2 3 4 
+#     print(i);
+
+def greet(name):
+    print("Hello,"+name);
+
+greet("Brij")
+
+# import this as t
+# t.d;
+# print('/n')
+# t
